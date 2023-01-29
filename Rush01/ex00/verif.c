e22cf0ac5e6d9ab9b632d7ee40c1e41c79583e98
Mathis Mac <Mathis Mac>
2023-01-29 17:30:48 +0100

    Real V1

--- Rush01/ex00/verif.c
+++ Rush01/ex00/verif.c
@@ -6,7 +6,7 @@
 /*   By: mamottet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 10:08:38 by mamottet          #+#    #+#             */
-/*   Updated: 2023/01/29 15:04:51 by mamottet         ###   ########.fr       */
+/*   Updated: 2023/01/29 15:41:27 by Onizukkka        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,15 @@ int	verif(char *str)
 	{
 		if (str[ii] <= '1' && str[ii] >= '4')
 		{
-			return (0);
+			return (1);
 		}
 		else
 			i ++;
 		ii ++;
 	}
 	if (i != 16)
-		return (0);
-	return (1);
+		return (1);
+	return (0);
 }
 void	make_perfect_int_tab(char *str,int *tab)
 {
@@ -55,28 +55,63 @@ void	make_perfect_int_tab(char *str,int *tab)
 	}
 }
 
+int check(int *tab)
+{
+	int i;
+	int ii;
+
+	i = 0;
+	ii = 4;
+
+	while (ii != 12)	 	 
+	{
+		if ((tab[i] + tab[ii]) <= 5)
+		{
+			printf("%d - %d - %d\n",tab[i],tab[ii],ii);
+			if (i == 3)
+			{
+				i = 12;
+			}
+			else
+			{
+				i ++;
+			}
+			ii ++;
+		}
+		else
+			return (1);
+	}
+	return (0);
+}
+
 int	main(int argc, char *argv[])
 {
 	int	i;
 	int	tab_int[16];
 	int	v;
+	int c; 
 
 	v = 0;
 	i = 0;
 	v = verif(argv[1]);
-	if (v == 1)
-		printf("Error1\n");
+
 	if (v == 2)
-		printf("Error2\n");
+		printf("Error VERIF \n");
 	else 
 	{
 		make_perfect_int_tab(argv[1],tab_int);
+	}
+	c = check(tab_int);
+	if (c == 1)
+	{
+		printf("Error CHECK");
+	}
+	else 
 		while (i != 16)
 		{
 			printf("%d",tab_int[i]);
 			i ++;
 		}
-	}
 	return (0);
 }
 
